4a59aea0f10189489956005e6603f4b01599a609
jacklyne <jacklyne.owuor@student.moringaschool>
2025-10-17 12:40:42 +0300

    added react-hooks-react-router-routes-lab-v6

---
+++ src/Index.js
@@ -0,0 +1,15 @@
+
+// src/index.jsx
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import routes from "./routes";
+import "./index.css";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={routes} />
+  </React.StrictMode>
+);
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,31 @@
 
+// src/routes.jsx
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Movie from "./pages/Movie.jsx";
+import Actors from "./pages/Actors.jsx";
+import Directors from "./pages/Directors.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
 
-const routes = [
-   
-  ];
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/movie/:id",
+    element: <Movie />,
+  },
+  {
+    path: "/actors",
+    element: <Actors />,
+  },
+  {
+    path: "/directors",
+    element: <Directors />,
+  },
+]);
 
 export default routes;
