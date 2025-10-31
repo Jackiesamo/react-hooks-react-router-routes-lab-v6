20cbe6421616897b2a14249733d460ed8e678179
jacklyne <jacklyne.owuor@student.moringaschool>
2025-10-31 13:23:37 +0300

    added another file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import routes from "./routes";
+
+const router = createBrowserRouter(routes);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
