efb521996ca4c105830a6dc2dce3a04508eb2c26
jacklyne <jacklyne.owuor@student.moringaschool>
2025-10-31 13:27:17 +0300

    added afile

---
+++ src/Routes.jsx
@@ -0,0 +1,14 @@
+// src/routes.jsx
+import Home from "./pages/Home";
+import Actors from "./pages/Actors";
+import Directors from "./pages/Directors";
+import Movie from "./pages/Movie";
+
+const routes = [
+  { path: "/", element: <Home /> },
+  { path: "/actors", element: <Actors /> },
+  { path: "/directors", element: <Directors /> },
+  { path: "/movie/:id", element: <Movie /> },
+];
+
+export default routes;
